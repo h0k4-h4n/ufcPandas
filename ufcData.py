d9a740fceccedd2bca6d79413e9b14095abd7201
ottoalves22 <ottoalves22@gmail.com>
2020-06-10 17:21:23 -0300

    data frame com vitórias

--- ufcData.py
+++ ufcData.py
@@ -1,6 +1,15 @@
 from pymongo import MongoClient
 from pathlib import Path
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+### funcoes auxiliares ###
+def pegarVitorias(linha):
+    for x in range(len(linha)):
+        if (linha)[x]=='-':
+            return int((linha)[x-2] +(linha)[x-1])
+            #return linha[x-1]
 
 f = open('settings.txt', 'r')
 
@@ -8,13 +17,21 @@ MONGO_URI = f.readline()
 #print(MONGO_URI)
 client = MongoClient(MONGO_URI)
 db = client['ufcData']
-manBantamWeight = db['manBantamWeight']
+fighters = db['manBantamWeight']
 
-p = (manBantamWeight.find()) #cursor
-#for x in p:
+p = (fighters.find()) #cursor
+toDf = []
+for x in p:
+    x['wins'] = pegarVitorias(x['category_position'])
+    toDf.append(x)
 #    print(x)
 
-df = pd.DataFrame(list(p))
+df = pd.DataFrame(toDf)
+df.drop('_id', axis=1, inplace=True)
+df.drop('category_position', axis=1, inplace=True)
+df.drop('nickname', axis=1, inplace=True)
+print(df)
+#print(df.describe()) #estatisticas sobre os lutadores
 
 client.close()
 #disconnect() #desconectando do banco
